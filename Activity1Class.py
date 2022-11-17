d9eee37d8d50942ac106aa10ac261ef49b71944f
Mikaela Grace <mikaelagrace@users.noreply.github.com>
2022-11-17 17:31:33 -0500

    Update Activity1Class.py
    no need to merge the Pull Request if you don't want to, it's just a way for me to suggest updates with inline diffs! or merge and remove my comments, whatever you prefer

--- Activity1Class.py
+++ Activity1Class.py
@@ -2,19 +2,34 @@
 Activity1Class for task1'''
 from transformers import pipeline
 import re
+from typing import List
+# Suggestions for the future, not required for this homework, just FYI: 
+# Start using pytype for type hints! really helpful for bugfinding 
+# https://docs.python.org/3/library/typing.html
+#     and
+# Start using black for autoformatting: 
+# https://github.com/psf/black
 
 class Sentiment_Analysis:
     def __init__(self,filename = "tiny_movie_reviews_dataset.txt"):
         print(f'\n{"":=^60}\n{"First Task":=^60}\n{"":=^60}\n')
-        self.filename = filename
-        text = open(self.filename)
-        lines = text.readlines()
-        cleanLines = []    
+        # dont need to save filename if you only use it here! 
+        
+        # you are not closing the file here! more info: 
+        # https://www.freecodecamp.org/news/with-open-in-python-with-statement-syntax-example/#:~:text=So%2C%20the%20open()%20function,the%20mode%2C%20and%20the%20encoding.
+        
+        with open(filename, "r") as f: 
+            lines = text.readlines()
+            self.cleaned_lines = self._clean_lines(lines)
+   
+    def _clean_lines(self, lines: List[str])
+        cleaned_lines = []   # camel case only used in class defs! not in vars
         for line in lines: 
             regex_sub = re.sub(r"[,.;\"@#?!&$]+", ' ', line)  
             regex_sub = re.sub(r"\s+", ' ', regex_sub)
-            cleanLines.append(regex_sub)
-        self.cleanLines = cleanLines
+            cleaned_lines.append(regex_sub)
+        )
+        return cleaned_lines
     
     def prediction(self,sentiment_model = pipeline("sentiment-analysis")):
         dictionary = sentiment_model(self.cleanLines)
